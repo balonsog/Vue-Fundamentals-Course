b0e1349689fb87905321e64f44059cbc6c6b77e9
Neila Estefhany Torres Panez <netorres@Neilas-MacBook-Pro.local>
2020-08-30 20:21:12 -0400

    feat(.md): Updates class1 challenge

--- challenges/class1/responsive-card/src/App.vue
+++ challenges/class1/responsive-card/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <img alt="Vue logo" src="./assets/photo-card.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>

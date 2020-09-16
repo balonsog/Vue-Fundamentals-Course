90e24e4f24a485273c7fb708243073779d88c868
Neila Estefhany Torres Panez <netorres@Neilas-MBP.home>
2020-09-16 09:43:11 +0200

    feat(.md): Updates class5 resources

---
+++ resources/class5/BaseButton.vue
@@ -0,0 +1,22 @@
+<template>
+  
+</template>
+
+<script>
+/**
+ * Requirements:
+ * - Button can display custom text
+ * - Button can display an icon to the right
+ * - Button can display an icon on either side
+ * - Replace content with a loading spinner
+ * - Replace an icon with a loading spinner
+ */
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+

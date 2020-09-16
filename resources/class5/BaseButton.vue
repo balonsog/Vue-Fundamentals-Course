ab6a89b35ea5e0f0b58bef2d1c9f525ef0f2f89e
Neila Estefhany Torres Panez <netorres@Neilas-MBP.home>
2020-09-16 10:59:14 +0200

    feat(.md): Updates class5 resources

--- resources/class5/BaseButton.vue
+++ resources/class5/BaseButton.vue
@@ -1,6 +1,55 @@
 <template>
-  
+  <button type="button" class=“nice-button“>
+    <LoadingSpinner v-if="isLoading" />
+    <template v-else>
+      <template v-if="iconLeftName">
+        <LoadingSpinner v-if="isLoadingLeft" />
+        <AppIcon v-else :icon=“iconLeftName” />
+      </template>
+      {{ text }}
+      <template v-if="iconRightName">
+        <LoadingSpinner v-if="isLoadingRight" />
+        <AppIcon v-else :icon=“iconRightName” />
+      </template>
+    </template>
+  </button>
 </template>
+
+<!-- Last solution
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+-->
+
+<!-- Second solution
+<template>
+  <button class="button">
+      <svg v-if="svgName" class="top"></svg>
+      {{text}}
+  </button>
+</template>
+
+<template>
+  <h1>My App</h1>
+  <BaseButton text="Submit" :showSVG="true"><AppIcon name="arrow-right" /></BaseButton>
+</template> 
+-->
+
+<!-- first solution
+<template>
+  <h1>My App</h1>
+  <BaseButton 
+    text="Submit" 
+    :isLoading="loading"
+    iconLeftName="left-arrow"
+    iconRightName="right-arrow"
+    :isLoadingLeft="loadingLeft"
+    :isLoadingRight="loadingRight"
+  />
+</template>
+-->
 
 <script>
 /**
@@ -12,11 +61,30 @@
  * - Replace an icon with a loading spinner
  */
 export default {
+    // props: {
+    //     text: {
+    //         type: String,
+    //         required: true
+    //     },
+    //     svgName: {
+    //         type: Boolean,
+    //         default: false
+    //     }
+    // }
+
+    props: [
+    'text', 
+    'iconLeftName', 
+    'iconRightName', 
+    'isLoading', 
+    'isLoadingLeft', 
+    'isLoadingRight'
+  ]
+}
 
 }
 </script>
 
 <style>
-
 </style>
 

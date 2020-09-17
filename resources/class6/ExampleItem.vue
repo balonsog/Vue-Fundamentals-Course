89fa255ce5315562e3af7b335e7f2324cf25f796
Neila Estefhany Torres Panez <netorres@Neilas-MBP.home>
2020-09-17 14:47:36 +0200

    feat(.md): Updates class6 resources

--- resources/class6/ExampleItem.vue
+++ resources/class6/ExampleItem.vue
@@ -3,19 +3,33 @@
         <h1>Advanced Props</h1>
         
         <div>{{ title }}</div>
+        <!-- <div>{{ id }} {{ title }}</div>-->
     </div>
 
     <!-- in App.vue -->
-    <!-- <props title="Title for my props" :is-published="false" /> -->
+    <!-- First solution-->
+    <props title="Title for my props" :is-published="false" :views="123" 
+                :id="post.id" :title="post.title"  /> 
+    <!-- Final solution-->
+    <props :is-published="false" :views="123" v-bind="post"/>
 </template>
 
 <script>
     module.exports = {
         props: {
             title: String,
-            isPublished: Boolean
+            isPublished: Boolean,
+            views: Number,
+            post: Object    // It's not really necessary 
         }
     }
+    /**
+     * App.vue
+     * post: {
+            id: 1,
+            title: 'My title',
+        }
+     */
 </script>
 
 <style>

a090243dac1adca383437887e01ea8a1dd8beb27
Neila Estefhany Torres Panez <netorres@Neilas-MBP.home>
2020-09-17 12:57:14 +0200

    feat(.md): Updates class6 resources

---
+++ resources/class6/c6WaRyuV/TestComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Hello from Component {{ test }}
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                test: 'test hello'
+            }
+        }
+    }
+</script>
+
+<style></style>

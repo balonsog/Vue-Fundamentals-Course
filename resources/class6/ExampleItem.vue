aeb72245a2c613ec01856b203744f979006e767b
Neila Estefhany Torres Panez <netorres@Neilas-MBP.home>
2020-09-17 14:36:38 +0200

    feat(.md): Updates class6 resources

---
+++ resources/class6/ExampleItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Advanced Props</h1>
+        
+        <div>{{ title }}</div>
+    </div>
+
+    <!-- in App.vue -->
+    <!-- <props title="Title for my props" :is-published="false" /> -->
+</template>
+
+<script>
+    module.exports = {
+        props: {
+            title: String,
+            isPublished: Boolean
+        }
+    }
+</script>
+
+<style>
+    
+</style>

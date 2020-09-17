8489cd4dd13cafe73358acab30144748bc417a77
Neila Estefhany Torres Panez <netorres@Neilas-MBP.home>
2020-09-17 14:32:28 +0200

    feat(.md): Updates class6 resources

---
+++ challenges/class6/todo-list/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+            <div>
+                <input type="text" placeholder="Add an item" v-model="item" />
+                <button @click.prevent="addItem">Add</button>
+                
+                <list :items="items"></list>
+            </div>
+        </div>
+</template>
+
+<script>
+import List from "./components/List.vue";
+
+export default {
+  name: "App",
+  components: {
+    List
+  },  
+  data: function () {
+        return {
+          item: '',
+          items: ['item1', 'item2']
+        }
+    },
+    methods: {
+        addItem: function () {
+            this.items.push(this.item);
+            // this.items.unshift(this.item);
+            this.item = '';
+        }
+    }
+};
+</script>
+
+<style>
+input {
+    padding: 5px 15px;
+    border-radius: 15px;
+}
+button {
+    text-transform: uppercase;
+    font-size: 14px;
+    margin-left: 5px;
+    font-weight: bold;
+}
+ul {
+    margin-top: 15px;
+}
+li {
+    padding: 5px 15px;
+    margin-bottom: 5px;
+    background: white;
+    border-radius: 15px;
+    color: #555;
+    display: flex;
+    justify-content: space-between;
+}
+</style>
---
+++ challenges/class6/todo-list/src/components/List.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+        <li v-for="item in items" :key="item.id">
+            {{ item }}
+        </li>
+    </ul>
+</template>
+
+<script>
+    module.exports = {
+        props: ['items']
+    }
+</script>
+
+<style></style>

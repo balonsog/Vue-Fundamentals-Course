73c5899fc72ed816062ddf3829313f9877576010
netorrespanez <ne.torrespanez@gmail.com>
2020-09-22 13:17:23 +0200

    feat(*): Updates class8 resources

---
+++ resources/class8/example-land-page/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <navigation></navigation>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navigation from "@/components/Navigation";
+import InputField from "@/components/InputField";
+
+export default {
+  name: "Home",
+  components: {
+    Navigation,
+    InputField
+  }
+};
+</script>

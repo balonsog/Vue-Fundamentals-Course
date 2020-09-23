79de18a298e97299ff8ecf578011c5a2fe36c885
netorrespanez <ne.torrespanez@gmail.com>
2020-09-23 13:45:35 +0200

    feat(.md): Updates class9 theory

--- resources/class9/vue-routing-example/src/App.vue
+++ resources/class9/vue-routing-example/src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <div id="app">
+    <NavBar/>
+    <router-view/>
+  </div>
+</template>
+
+
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+<!-- 
+
+<template>
+  <div id="app">
     <div id="nav">
       <router-link :to="{ name: 'event-list' }">List</router-link> |
-      <!-- <router-link :to="{ name: 'event-show' }">Show Event #1</router-link> | -->
+      <router-link :to="{ name: 'event-show' }">Show Event #1</router-link> |
       <router-link :to="{ name: 'event-create' }">Create</router-link> |
       <router-link :to="{ name: 'user', params: { username: 'Steph'} }">Steph</router-link>
 
     </div>
     <router-view/>
   </div>
-</template>
+</template>-->
 
 <style>
 html {
--- resources/class9/vue-routing-example/src/views/EventList.vue
+++ resources/class9/vue-routing-example/src/views/EventList.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard/>
+    <BaseIcon/>
     <!-- <router-link :to="{ name: 'event-show', params: { id: '1' } }">Show Event #1</router-link> -->
   </div>
 </template>
